06cf94f7b9e3145d4060e2606d4fde56939261ed
alexheil <aheil4250@gmail.com>
2019-03-29 14:24:48 -0400

    elements

--- db/migrate/20190329181637_create_elements.rb
+++ db/migrate/20190329181637_create_elements.rb
@@ -1,6 +1,10 @@
 class CreateElements < ActiveRecord::Migration[5.0]
   def change
     create_table :elements do |t|
+      t.string :title
+      t.text :description
+      t.string :image
+      t.string :slug
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190329172105) do
+ActiveRecord::Schema.define(version: 20190329181637) do
+
+  create_table "elements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image"
+    t.string   "slug"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
