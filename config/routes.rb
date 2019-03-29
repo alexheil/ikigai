75e5d8ca15374c300bdf609e7e7937dbb0fb2a63
alexheil <aheil4250@gmail.com>
2019-03-29 13:26:21 -0400

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root 'static_pages#home'
+
+  devise_for :users, controllers: { sessions: "users/sessions", passwords: "users/passwords", registrations: "users/registrations", confirmations: "users/confirmations",  unlocks: "users/unlocks"}
+
+  devise_scope :user do
+    get 'sign_out', to: 'users/sessions#destroy', path: 'sign-out'
+    get 'sign_in', to: 'users/sessions#new', path: 'sign-in'
+    get 'register', to: 'users/registrations#new'
+  end
+
+  resources :users, controller: 'users/users', only: :show
 
 end
